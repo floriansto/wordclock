325e13d9e7de098db789458185d788cf0e1075ba
Florian Stockburger <stockburger.florian@gmail.com>
2022-03-26 11:03:22 +0100

    Fix some dialect words

--- includes/main.h
+++ includes/main.h
@@ -30,13 +30,13 @@ enum class ClockStr {
   F(ClockStr::Zero,        0,  true,  "NULL", "NULL") \
   F(ClockStr::One,         1,  true,  "EINS", "EINSE") \
   F(ClockStr::Two,         2,  true,  "ZWEI", "ZWEIE") \
-  F(ClockStr::Three,       3,  true,  "DREI", "DREIE") \
+  F(ClockStr::Three,       3,  true,  "DREI", "DRII") \
   F(ClockStr::Four,        4,  true,  "VIER", "VIERE") \
   F(ClockStr::Five,        5,  true,  "FÜNF", "FÜNFE") \
   F(ClockStr::Six,         6,  true,  "SECHS", "SECHSE") \
   F(ClockStr::Seven,       7,  true,  "SIEBEN", "SIEBENE") \
-  F(ClockStr::Eight,       8,  true,  "ACHT", "ACHTE") \
-  F(ClockStr::Nine,        9,  true,  "NEUN", "NEUNE") \
+  F(ClockStr::Eight,       8,  true,  "ACHT", "ACHDE") \
+  F(ClockStr::Nine,        9,  true,  "NEUN", "NIINE") \
   F(ClockStr::Ten,         10, true,  "ZEHN", "ZEHNE") \
   F(ClockStr::Eleven,      11, true,  "ELF", "ELFE") \
   F(ClockStr::Twelve,      12, true,  "ZWÖLF", "ZWÖLFE") \
